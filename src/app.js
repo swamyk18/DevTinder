3232b1596f1d32a2154f3e8066c5f25570892c52
kummariswamy18 <160798855+kummariswamy18@users.noreply.github.com>
2025-09-25 19:01:47 +0530

    Exploring Api methods

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express=require("express");
 const app=express();
 
-
-app.use(("/hello",(req,res)=>{
-    res.send("Namaste Swamy");
-}));
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Swamy"});
+})
+app.post("/user",(req,res)=>{
+    // need to connect to database
+    res.send("successfully connected to databse")
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted ")
+})
 app.use(("/test",(req,res)=>{
     res.send("hello");
 }));
-
+app.use("/user",(req,res)=>{
+    res.send("hahaha")
+})
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
 });

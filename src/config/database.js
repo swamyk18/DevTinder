90f43af8ba4dd6cfa5c5f595118168edb14dbd0e
Kummari Swamy <swamyswamy62162@gmail.com>
2026-09-10 16:50:50 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 const ConnectDb=async ()=>{
-  await  mongoose.connect("mongodb+srv://swamy18:Cmrec%401234@namastenodejs.aekyj89.mongodb.net/devTinder")
+  await  mongoose.connect("XXX")
 }
 module.exports=ConnectDb
